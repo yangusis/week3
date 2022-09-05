ef3a7f59edec73ae30c90e2b93d67178ecc0f6d1
yangusis <yangusis@gmail.com>
2022-09-05 03:50:53 -0700

    kind of finished arraymethods, ran into one problem on last one

--- arrayMethods.js
+++ arrayMethods.js
@@ -61,9 +61,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
-// CODE HERE
-//const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+function redCb(sum, num) {
+  return sum - num
+}
+const totalPopulation = populations.reduce(redCb)
+console.log(totalPopulation)
 
 
 ////////// PROBLEM 4 //////////
@@ -86,9 +88,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
-// CODE HERE
-//const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
+function monFilter(cond) {
+  return cond.CP > 200
+}
+const myStrongest = monstersInYourPocket.filter(monFilter)
+console.log(myStrongest)
 
 
 ////////// PROBLEM 5 //////////
@@ -104,8 +108,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-// CODE HERE
-
+function totalTax(cond) {
+  return cond.price + cond.price * cond.tax
+}
+const totalOrders = orders.map(totalTax)
+console.log(totalOrders)
 
 
 ////////// PROBLEM 6 //////////
@@ -124,4 +131,10 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method(s) to get the sum of bobsTotal.
 */
 
-// CODE HERE
+// couldn't get this one to work, just throws the error "cannot read properties of undefined, which I assume the object defines the owner part as undefined for some reason"
+function isBob(cond) {
+  if(cond.owner === "Bob")
+  return cond.price
+}
+const totalBob = purchases.reduce(isBob)
+console.log(totalBob)
